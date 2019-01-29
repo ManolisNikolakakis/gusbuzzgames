b0472acdd6b380a820d90be71db4b3cb289f467b
Theo Alexiou <theofilosalexiou@gmail.com>
2019-01-29 06:40:47 +0100

    Added accepted buttons

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # gusbuzzgames
 Gusbuzz
 
-POU SAI TRELE MOU
-
 Video Games:
 
 Rhythm Game:
@@ -10,6 +8,3 @@ Rhythm Game:
 For Start:
 Make 9 input buttons, game randomizes input required with 2 second frame for input
 Add score
-
-Notes:
-Currently an example main file has been created in Pyton 2.7, to be ported in Python 3.6 once installations problems are fixed
--- gusbuzz/assets/settings.py
+++ gusbuzz/assets/settings.py
@@ -1,3 +1,4 @@
+import pygame
 
 # SCREEN #
 
@@ -13,3 +14,43 @@ MAIN_TUNE = 'resources/sounds/Tune1.mp3'
 
 BACKGROUND_IMAGE = 'resources/images/game_image.jpg'
 BACKGROUND_IMAGE_STARTING_POINT = (0, 0)  # Always start from the top
+
+
+# DIFFICULTY #
+
+DIFFICULTY = {
+    'easy': (
+        pygame.K_d,
+        pygame.K_f,
+        pygame.K_j,
+        pygame.K_k,
+    ),
+    'medium': (
+        pygame.K_a,
+        pygame.K_s,
+        pygame.K_d,
+        pygame.K_f,
+        pygame.K_h,
+        pygame.K_j,
+        pygame.K_k,
+        pygame.K_l,
+    ),
+    'hard': (
+        pygame.K_q,
+        pygame.K_w,
+        pygame.K_a,
+        pygame.K_s,
+        pygame.K_p,
+        pygame.K_o,
+        pygame.K_l,
+        pygame.K_k,
+        pygame.K_c,
+        pygame.K_v,
+        pygame.K_n,
+        pygame.K_m,
+        pygame.K_g,
+        pygame.K_h,
+        pygame.K_t,
+        pygame.K_b,
+    )
+}
