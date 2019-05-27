1f464cd2f76d78ffca7225b2b71c21b22583d8cc
ManolisNikolakakis <manolis-nikolakakis@hotmail.com>
2019-05-27 13:50:03 +0200

    functional splash screen

--- gusbuzz/main.py
+++ gusbuzz/main.py
@@ -20,26 +20,27 @@ font = pygame.font.Font(None, 36)
 # Functions used in the game
 
 
-def load_settings():
+def load_settings(screen):
 	# pygame.display.set_icon(surface)
-    screen = pygame.display.set_mode(settings.SIZE)
     screen.fill(settings.BLACK)
     pygame.display.set_caption("Some Video Game")
 
-def load_splash_screen():
-    text = font.render("Current Score", True, settings.WHITE)
-    screen.blit(text, [10, 10])
+def load_splash_screen(screen):
 
-    text = font.render("Presented by", True, settings.WHITE)
-    screen.blit(text, [450, 300])
+    text = font.render("Presented by...", True, settings.WHITE)
+    screen.blit(text, [350, 270])
 
-    text = font.render("GusBuzzGames", True, settings.RED)
-    screen.blit(text, [450, 330])
+    text = font.render("GusBuzzGames", True, settings.WHITE)
+    screen.blit(text, [350, 300])
+
+    pygame.display.flip()
 
     time.sleep(3)
     
     text = font.render("PRESS F TO CONTINUE", True, settings.RED)
-    screen.blit(text, [450, 420])      
+    screen.blit(text, [310, 380])      
+
+    pygame.display.flip()
 
     while True:
         for event in pygame.event.get():
@@ -48,8 +49,10 @@ def load_splash_screen():
 
 # Game Script Starting Point
 
-load_settings()
-load_splash_screen()
+screen = pygame.display.set_mode(settings.SIZE)
+
+load_settings(screen)
+load_splash_screen(screen)
 
 pygame.mixer.init()
 pygame.mixer.music.load(settings.MAIN_TUNE)
@@ -57,17 +60,18 @@ pygame.mixer.music.play(settings.INFINITE_MUSIC_LOOP)
 
 backGround = BackGround(settings.BACKGROUND_IMAGE, settings.BACKGROUND_IMAGE_STARTING_POINT)
 
-text = font.render("Current Score", True, settings.WHITE)
-screen.blit(text, [10, 10])
- 
-text = font.render(score, True, settings.WHITE)
-screen.blit(text, [10, 40])
-
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         screen.fill(settings.BLACK)
         screen.blit(backGround.image, backGround.rect)
+
+        text = font.render("Current Score", True, settings.WHITE)
+        screen.blit(text, [10, 10])
+ 
+        text = font.render(score, True, settings.WHITE)
+        screen.blit(text, [10, 40])
+        
         pygame.display.flip()
 
